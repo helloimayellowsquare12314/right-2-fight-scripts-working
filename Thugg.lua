bee2ee20a980e315810cec38951c5e076bf95e29
helloimayellowsquare12314 <helloimayellowsquare@gmail.com>
2025-12-19 02:53:30 -0300

    Add files via upload

---
+++ Thugg.lua
@@ -0,0 +1,28 @@
+﻿local Thug = game.ReplicatedStorage.Styles.Rush
+Thug.VisualName.Value = "Thug"
+Thug.Color.Value = Color3.fromRGB(0, 90, 255)
+
+Thug.Rush1.Value = "龍Attack1"
+Thug.Rush2.Value = "龍Attack3"
+Thug.Rush3.Value = "BStrike3"
+Thug.Rush4.Value = "BEvadeStrikeRight"
+Thug.Rush5.Value = "龍Attack2"
+Thug.Rush6.Value = "龍Attack1"
+Thug.Rush7.Value = "B2Strike1"
+Thug.Rush8.Value = "RDashAttack"
+
+Thug.Strike1.Value = "GuruKnockback"
+Thug.Strike2.Value = "RDashAttack"
+Thug.Strike3.Value = "龍2Strike1"
+Thug.Strike4.Value = "龍Attack2"
+Thug.Strike5.Value = "RDasbAttack"
+Thug.Strike6.Value = "龍2Strike4"
+Thug.Strike7.Value = "RDasbAttack"
+Thug.Strike8.Value = "RSweep"
+Thug.Strike9.Value = "RDasbAttack"
+
+
+Thug.Grab.Value = "GGrab"
+Thug.Taunt.Value = "DragonTaunt"
+Thug.DashAttack.Value = "RDashAttack"
+Thug.GrabStrike.Value = "T_CounterQuickstep"
